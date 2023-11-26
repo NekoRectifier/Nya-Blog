94b66f63ab97058f98a2237e60d1c8ed902ae3bc
NekoRectifier <hyx123250@gmail.com>
2023-11-26 11:16:30 +0800

    update: theme & add umami tracker

--- themes/vivia/scripts/categories-process.js
+++ themes/vivia/scripts/categories-process.js
@@ -1,6 +1,6 @@
 const url_for = hexo.extend.helper.get('url_for').bind(hexo);
 
-function build_tag_tree(tree) {
+function build_tag_tree(tree, currDepth, maxDepth) {
     let ret = '<div class="category-box">';
     for(let item of tree) {
         ret += `
@@ -9,9 +9,9 @@ function build_tag_tree(tree) {
                 <div class="category-count">${item.length}</div>
             </a>
         `;
-        if ('children' in item && item.children.length>0) {
+        if ('children' in item && item.children.length>0 && currDepth < maxDepth) {
             ret += '<div class="children">'
-            ret += build_tag_tree(item.children);
+            ret += build_tag_tree(item.children, currDepth+1, maxDepth);
             ret += '</div>'
         }
     }
@@ -39,8 +39,15 @@ function list_to_tree(list) {
 }
 
 hexo.on('generateAfter', function(post){
+    let depth = Infinity;
+    // hexo.config.theme_config 更改为 hexo.theme，不然会报错 theme_config 不存在
+    if (hexo.theme.categories && hexo.theme.categories.max_depth
+            && hexo.theme.categories.max_depth > 0) {
+        depth = hexo.theme.categories.max_depth;
+        console.log("depth: " + depth);
+    }
     let tree = list_to_tree(hexo.locals.get("categories").data);
-    let cate_tree = build_tag_tree(tree);
+    let cate_tree = build_tag_tree(tree, 1, depth);
     hexo.extend.helper.register('categories_tree', function() {
         return cate_tree;
     })
